21a8c4d331cf93bb79b2b60e311a79ae957acace
PavelKursakov <pavelkursakov96@gmail.com>
2021-12-23 15:41:49 +0300

    first commit

---
+++ src/Runner.java
@@ -0,0 +1,37 @@
+import by.epam.lab.BusinessTrip;
+
+public class Runner {
+    public static void main(String[] args) {
+        BusinessTrip[] businessTrips = {
+                new BusinessTrip("Pavel", 1540, 5),
+                new BusinessTrip("Vlad", 1710, 7),
+                null,
+                new BusinessTrip("Kirill", 734, 11),
+                new BusinessTrip("Vova", 2120, 3),
+                new BusinessTrip()
+        };
+        int maxTotal = 0;
+        int count = 0;
+        int index = 0;
+        for (BusinessTrip businessTrip : businessTrips) {
+            if (businessTrip == null) {
+                count++;
+                continue;
+            }
+            businessTrip.show();
+            if (businessTrip.getTotal() > maxTotal) {
+                maxTotal = businessTrip.getTotal();
+                index = count;
+            }
+            count++;
+        }
+        int daysFistTrip = businessTrips[0].getNumberOfDays();
+        int daySecondTrip = businessTrips[1].getNumberOfDays();
+        System.out.println(businessTrips[index]);
+        businessTrips[businessTrips.length - 1].setTransportationExpenses(211);
+        System.out.println(daysFistTrip + daySecondTrip);
+        for (BusinessTrip businessTrip : businessTrips) {
+            System.out.println(businessTrip);
+        }
+    }
+}

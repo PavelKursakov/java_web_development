79a5e1d61710f784c29acb9950681d387107c9bb
PavelKursakov <96572353+PavelKursakov@users.noreply.github.com>
2022-01-07 14:21:03 +0300

    Update DiscountPurchase.java

--- src/by/epam/lab/DiscountPurchase.java
+++ src/by/epam/lab/DiscountPurchase.java
@@ -32,7 +32,7 @@ public class DiscountPurchase extends Purchase {
     public Byn getCost() {
         Byn byn = new Byn(super.getCost());
         if (getNumberOfUnits() > UNIT_NUMBER) {
-            byn.mul(1 - discountPercent / 100,RoundMethod.ROUND,0);
+            byn.mul(1 - discountPercent / 100, RoundMethod.ROUND, 0);
         }
         return byn;
     }

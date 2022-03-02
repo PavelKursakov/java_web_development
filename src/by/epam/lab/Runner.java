276de012ece291102dbc3814c5ecaf5798f7af0c
PavelKursakov <pavelkursakov96@gmail.com>
2022-03-02 14:37:40 +0300

    second commit

--- src/by/epam/lab/Runner.java
+++ src/by/epam/lab/Runner.java
@@ -10,57 +10,31 @@ import static by.epam.lab.constants.Constants.*;
 
 public class Runner {
     public static void main(String[] args) {
-        try {
-            Class.forName(CLASS_NAME);
-            Connection cn = null;
-            Statement st = null;
-            ResultSet rs = null;
-            PreparedStatement ps = null;
-            try {
-                cn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-                st = cn.createStatement();
-                rs = st.executeQuery(SELECT_LEN_NUM);
+        try (Connection cn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+             Statement st = cn.createStatement();
+             PreparedStatement ps = cn.prepareStatement(INSERT_INTO_FR_TABLE)) {
+            try (ResultSet rs = st.executeQuery(SELECT_LEN_NUM)) {
                 List<LenNum> lenNumList = new ArrayList<>();
                 while (rs.next()) {
-                    lenNumList.add(new LenNum(rs.getInt(LEN_ID), rs.getInt(NUM_ID)));
+                    LenNum lenNum = new LenNum(rs.getInt(LEN_ID), rs.getInt(NUM_ID));
+                    lenNumList.add(lenNum);
+                    System.out.println(lenNum);
                 }
-                ps = cn.prepareStatement(INSERT_INTO_FR_TABLE);
-                ps.executeUpdate(DELETE_ALL_FROM_FREQUENCIES);
+                st.executeUpdate(DELETE_ALL_FROM_FREQUENCIES);
                 for (LenNum lenNum : lenNumList) {
                     ps.setInt(LEN_ID, lenNum.getLen());
                     ps.setInt(NUM_ID, lenNum.getNum());
-                    ps.executeUpdate();
+                    ps.addBatch();
                 }
-                rs = ps.executeQuery(SELECT_LEN_NUM_FROM_FREQUENCIES);
-                printLenNum(rs, LEN_ID, NUM_ID);
-                rs = ps.executeQuery(SELECT_LEN_NUM_AFTER_SORTING);
-                printLenNum(rs, LEN_ID, NUM_ID);
-            } finally {
-                if (rs != null && !rs.isClosed()) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (st != null) {
-                    st.close();
-                }
-                if (cn != null) {
-                    cn.close();
-                }
+                ps.executeBatch();
             }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void printLenNum(ResultSet resultSet, int lenId, int numId) {
-        try {
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt(lenId) + DELIMITER + resultSet.getInt(numId));
+            try (ResultSet rs = st.executeQuery(SELECT_LEN_NUM_AFTER_SORTING)) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt(LEN_ID) + DELIMITER + rs.getInt(NUM_ID));
+                }
             }
         } catch (SQLException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
--- src/by/epam/lab/beans/LenNum.java
+++ src/by/epam/lab/beans/LenNum.java
@@ -3,11 +3,9 @@ package by.epam.lab.beans;
 import by.epam.lab.constants.Constants;
 
 public class LenNum {
-    private int len;
-    private int num;
 
-    public LenNum() {
-    }
+    private final int len;
+    private final int num;
 
     public LenNum(int len, int num) {
         this.len = len;
@@ -18,16 +16,8 @@ public class LenNum {
         return len;
     }
 
-    public void setLen(int len) {
-        this.len = len;
-    }
-
     public int getNum() {
         return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
     }
 
     @Override
